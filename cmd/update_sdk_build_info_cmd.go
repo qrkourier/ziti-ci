aa8fb454f3091f6bd5408b239900ec22d2de8a68
Paul Lorenz <paul.lorenz@netfoundry.io>
2024-05-16 13:18:54 -0400

    Fix copyright templates

--- cmd/update_sdk_build_info_cmd.go
+++ cmd/update_sdk_build_info_cmd.go
@@ -25,7 +25,7 @@ import (
 )
 
 var goSdkBuildInfoTemplate = `/*
- * Copyright NetFoundry, Inc.
+ * Copyright NetFoundry Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
